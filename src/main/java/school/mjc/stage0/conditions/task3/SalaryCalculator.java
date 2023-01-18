6823cf0785b8b666107f91d4ae0b233e308ebe57
Assetjan <asetziyabek@gmail.com>
2023-01-19 00:23:07 +0600

    Done

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,19 +1,21 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {//Basic Salary > 20000 : taxes - 20% Basic salary < 0 -> "wrong input!"
+    public void calculateSalary(int salary) {
+        //Basic Salary > 20000 : taxes - 20% Basic salary < 0 -> "wrong input!"
+        double salary1;
         if(salary > 0) {
             if (salary <= 10000){
-                salary = salary - ((salary * 15) / 100);
-                System.out.println(salary);
+                salary1 = salary - ((salary * 15) / 100);
+                System.out.println(salary1);
             }
             else if (salary > 10000 && salary <= 20000){
-                salary = salary - ((salary * 18) / 100);
-                System.out.println(salary);
+                salary1 = salary - ((salary * 18) / 100);
+                System.out.println(salary1);
             }
             else if (salary > 20000){
-                salary = salary - ((salary * 20) / 100);
-                System.out.println(salary);
+                salary1 = salary - ((salary * 20) / 100);
+                System.out.println(salary1);
             }
         }
         else {
